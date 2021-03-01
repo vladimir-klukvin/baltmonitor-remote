5ce22cf8a367a59525abe178c0a4fb37d55c36b4
Klukvin <vladimir.klukvin@yandex.ru>
2021-03-01 10:57:14 +0300

    Add comments to session header

--- src/session.h
+++ src/session.h
@@ -10,9 +10,12 @@
 #ifndef SESSION_H_
 #define SESSION_H_
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
+/**
+ * @brief Struct which contain state of pair communication session
+ */
 struct session_info {
     uint16_t session_id;
     bool is_host_connected;
@@ -21,12 +24,30 @@ struct session_info {
     int32_t target_sockfd;
 };
 
+/**
+ * @brief Get session by specified id
+ * @param id Id number of session
+ * @return struct session_info* Pointer to requested session
+ */
 struct session_info *session_get(uint16_t id);
 
+/**
+ * @brief Add new session with specified id
+ * @param session Session to store
+ * @param id Id of session
+ */
 void session_add(struct session_info session, uint16_t id);
 
+/**
+ * @brief Remove session by id. If session not found does nothing.
+ * @param id Id of session
+ */
 void session_remove(uint16_t id);
 
+/**
+ * @brief Initialize sessions table to store max_sessions
+ * @param max_sessions Max number of sessions which server supports
+ */
 void session_init_table(int16_t max_sessions);
 
 #endif /* SESSION_H_ */
